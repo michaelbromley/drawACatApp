315cde0896e4c2d88e25edb236e30152c55a273f
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-08 06:43:41 +0100

    added cat and behaviour services

---
+++ src/common/services/behaviour.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Michael on 08/03/14.
+ */
+angular.module('drawACat.common.services.behaviour', [])
+
+/**
+ * A behaviour object described how a part should react to events such as user input.
+ */
+    .factory('behaviour', function() {
+
+    }
+);
