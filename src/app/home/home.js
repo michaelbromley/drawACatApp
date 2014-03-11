67182907a7e9adc4afbdd18ae4a25190a0757bf7
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-11 09:07:58 +0100

    added simple home view with a list of cats

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,33 +1,15 @@
-/**
- * Each section of the site has its own module. It probably also has
- * submodules, though this boilerplate is too simple to demonstrate it. Within
- * `src/app/home`, however, could exist several additional folders representing
- * additional modules that would then be listed as dependencies of this one.
- * For example, a `note` section could have the submodules `note.create`,
- * `note.delete`, `note.edit`, etc.
- *
- * Regardless, so long as dependencies are managed correctly, the build process
- * will automatically take take of the rest.
- *
- * The dependencies block here is also where component dependencies should be
- * specified, as shown below.
- */
-angular.module( 'ngBoilerplate.home', [
-  'ui.state',
-  'plusOne'
+
+angular.module( 'drawACat.home', [
+        'ui.state'
 ])
 
-/**
- * Each section or module of the site can also have its own routes. AngularJS
- * will handle ensuring they are all available at run-time, but splitting it
- * this way makes each module more "self-contained".
- */
+
 .config(function config( $stateProvider ) {
   $stateProvider.state( 'home', {
     url: '/home',
     views: {
       "main": {
-        controller: 'HomeCtrl',
+        controller: 'HomeController',
         templateUrl: 'home/home.tpl.html'
       }
     },
@@ -38,7 +20,11 @@ angular.module( 'ngBoilerplate.home', [
 /**
  * And of course we define a controller for our route.
  */
-.controller( 'HomeCtrl', function HomeController( $scope ) {
+.controller( 'HomeController', function HomeController( $scope, datastore ) {
+
+        datastore.listCats().success(function(data) {
+            $scope.cats = data;
+        });
 })
 
 ;
