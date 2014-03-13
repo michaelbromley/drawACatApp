b8f5c3115fbd6490cccd1bad42de9b7e885d70a8
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-13 13:39:33 +0100

    centralised the directives and services modules in the cat module

---
+++ src/app/cat/directives/_directives.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Michael on 13/03/14.
+ */
+angular.module('drawACat.cat.directives', []);
---
+++ src/app/cat/services/_services.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Michael on 12/03/14.
+ */
+angular.module('drawACat.cat.services', []);
