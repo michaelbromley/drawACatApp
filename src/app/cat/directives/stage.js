8c2bc4c893b95ab6be51cc5ce33863c8c590fddb
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-14 20:54:30 +0100

    made full screen canvas when viewing cat

--- src/app/cat/directives/stage.js
+++ src/app/cat/directives/stage.js
@@ -14,29 +14,44 @@ angular.module('drawACat.cat.directives')
                 cat: '='
             },
             link: function(scope, element) {
+                var canvas = document.getElementById('stage');
+                actuator.init(scope.cat);
+                var _renderer = renderer.Init(canvas);
+                scope.x = 0;
+                scope.y = 0;
+
+                function resizeCanvas() {
+                    var windowWidth = $window.innerWidth;
+                    var windowHeight = $window.innerHeight;
+                    canvas.width = windowWidth;
+                    canvas.height = windowHeight;
+
+                    // move the cat to be in the centre and at the bottom of the canvas
+                    var catWidth = scope.cat.getWidth();
+                    var catHeight = scope.cat.getHeight();
+                    var xAdjustment = (windowWidth / 2) - (catWidth / 2);
+                    var yAdjustment = windowHeight - (catHeight + 20);
+                    scope.cat.adjustPosition(xAdjustment, yAdjustment);
+                }
+                resizeCanvas();
+                angular.element($window).bind('resize', function() {
+                    resizeCanvas();
+                });
 
                 var rafId;
-                actuator.init(scope.cat);
-
                 var renderFrame = function() {
                     _renderer.clearCanvas();
                     transformer.transform(scope.cat, scope.x, scope.y);
                     _renderer.renderCat(scope.cat);
                     rafId = $window.requestAnimationFrame(renderFrame);
                 };
-
+                renderFrame();
 
                 $document.on('mousemove', function(e) {
-                    var stageRect = element[0].getBoundingClientRect();
+                    var stageRect = canvas.getBoundingClientRect();
                     scope.x = e.clientX - stageRect.left;
                     scope.y = e.clientY - stageRect.top;
                 });
-
-                var _renderer = renderer.Init(element.children()[0]);
-                scope.x = 0;
-                scope.y = 0;
-
-                renderFrame();
 
                 scope.$on("$destroy", function() {
                     if (rafId) {
@@ -44,7 +59,6 @@ angular.module('drawACat.cat.directives')
                         actuator.destroy();
                     }
                 });
-
             }
         };
     }
