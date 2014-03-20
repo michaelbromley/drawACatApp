a47f1c09d6073b8884b5323da8e8d280c481976a
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-20 14:33:20 +0100

    refactored route handlers into anonymous functions

--- api/index.php
+++ api/index.php
@@ -7,81 +7,101 @@
  */
 
 include('Slim/Slim.php');
+include('thumbnail.php');
+
 use Slim\Slim;
 \Slim\Slim::registerAutoloader();
 
 $app = new Slim();
-
-
+$db = getConnection();
 
 /**
  * Define the routes of the API
  */
-$app->get('/cat/', 'listCats');
-$app->get('/cat/:id', 'loadCat');
-$app->post('/cat/', 'addCat');
-
-$app->run();
-
-function listCats() {
-
+$app->get('/cat/', function() use($app, $db) {
 	$sql = "SELECT id, name, description, rating FROM cats";
 
 	try {
-		$db = getConnection();
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		echo json_encode($result);
-	} catch(PDOException $e) {
-		echo '{"error":{"text":'. $e->getMessage() .'}}';
-	}
-}
 
-function loadCat($id) {
+		$response = $app->response();
+		$response['Content-Type'] = 'application/json';
+		$response->status(200);
+		$response->body(json_encode($result));
+	} catch(PDOException $e) {
+		respondError($e->getMessage());
+	}
+});
+
+$app->get('/cat/:id', function($id) use($app, $db) {
 	$sql = "SELECT * FROM cats WHERE id = :id";
 
-		try {
-			$db = getConnection();
-			$stmt = $db->prepare($sql);
-			$stmt->bindParam("id", $id);
-			$stmt->execute();
-			$result = $stmt->fetch(PDO::FETCH_ASSOC);
-			$dataDecoded = json_decode($result['data']);
-			$result['data'] = $dataDecoded;
-			echo json_encode($result);
-		} catch(PDOException $e) {
-			echo '{"error":{"text":'. $e->getMessage() .'}}';
-		}
-}
+	try {
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		$dataDecoded = json_decode($result['data']);
+		$result['data'] = $dataDecoded;
 
-function addCat() {
+		$response = $app->response();
+		$response['Content-Type'] = 'application/json';
+		$response->status(200);
+		$response->body(json_encode($result));
+	} catch(PDOException $e) {
+		respondError($e->getMessage());
+	}
+});
+
+$app->post('/cat/', function() use($app, $db) {
 	$request = Slim::getInstance()->request();
 	$data = json_decode($request->getBody());
 
-	$sql = "INSERT INTO cats (name, description, data) VALUES (:name, :description, :data)";
+	$thumbnail = new Thumbnail($data->thumbnail);
+	$thumbnail->save();
+	$thumbnailFileName = $thumbnail->getFileName();
+
+	$sql = "INSERT INTO cats (name, description, data, author, isPublic, thumbnail) VALUES (:name, :description, :data, :author, :isPublic, :thumbnail)";
+	$isPublic = $data->isPublic === "true" ? 1 : 0;
 	try {
-		$db = getConnection();
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("name", $data->name);
 		$stmt->bindParam("description", $data->description);
 		$stmt->bindParam("data", json_encode($data->cat));
+		$stmt->bindParam("author", $data->author);
+		$stmt->bindParam("isPublic", $isPublic);
+		$stmt->bindParam("thumbnail", $thumbnailFileName);
 		$stmt->execute();
-		$data->id = $db->lastInsertId();
-		$db = null;
-		echo json_encode($data);
+
+		$responseData = array(
+			"id" => $db->lastInsertId()
+		);
+
+		$response = $app->response();
+		$response['Content-Type'] = 'application/json';
+		$response->status(201);
+		$response->body(json_encode($responseData));
 	} catch(PDOException $e) {
-		echo '{"error":{"text":'. $e->getMessage() .'}}';
+		respondError($e->getMessage());
 	}
+});
+$app->run();
+
+function respondError($errorMessage) {
+	$response = Slim::getInstance()->response();
+	$response['Content-Type'] = 'application/json';
+	$response->status(400);
+	$response->body(json_encode($errorMessage));
 }
 
-
 function getConnection() {
-    $dbhost="127.0.0.1";
-    $dbuser="root";
-    $dbpass="";
-    $dbname="drawacat";
-    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbh;
+	$dbhost="127.0.0.1";
+	$dbuser="root";
+	$dbpass="";
+	$dbname="drawacat";
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
 }
