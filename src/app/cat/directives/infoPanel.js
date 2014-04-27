a6dbb7e04ef0aaaafb4a31a9c50ca4560ca14d67
Michael Bromley <michael@michaelbromley.co.uk>
2014-04-27 10:11:28 +0200

    created infoPanel directive

---
+++ src/app/cat/directives/infoPanel.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Michael on 26/04/14.
+ */
+
+angular.module('drawACat.cat.directives')
+
+    .directive('dacInfoPanel', function($timeout, $window) {
+
+        return {
+            restrict: 'AE',
+            templateUrl: 'cat/directives/infoPanel.tpl.html',
+            replace: true,
+            transclude: true,
+            scope: true,
+            link: function(scope, element, attrs) {
+                scope.infoPanelVisible = true;
+
+                scope.toggleInfoPanel = function() {
+                    if (scope.infoPanelVisible) {
+                        contract();
+                    } else {
+                        expand();
+                    }
+                };
+                $timeout(contract, 5000);
+
+                angular.element($window).bind('resize', function() {
+                    if (!scope.infoPanelVisible) {
+                        contract();
+                    }
+                });
+
+                function contract() {
+                    var panelHeight = element[0].offsetHeight;
+                    element.css('top', -panelHeight + 40 + 'px');
+                    scope.infoPanelVisible = false;
+                }
+
+                function expand() {
+                    element.css('top', null);
+                    scope.infoPanelVisible = true;
+                }
+            }
+        };
+    });
