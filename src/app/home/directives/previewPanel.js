95d7179be14161830d43c936be5c72ac47f79c9b
Michael Bromley <michael@michaelbromley.co.uk>
2014-05-01 08:19:53 +0100

    pretty nice highlight effect for preview-panel - will attempt to simplify

--- src/app/home/directives/previewPanel.js
+++ src/app/home/directives/previewPanel.js
@@ -6,14 +6,49 @@
 
 angular.module('drawACat.home.previewPanel', [])
 
-.directive('dacPreviewPanel', function() {
+    .directive('dacPreviewPanel', function($rootScope) {
 
-    return {
-        restrict: 'AE',
-        templateUrl: 'home/directives/previewPanel.tpl.html',
-        scope: {},
-        link: function(scope, element, attrs) {
+        return {
+            restrict: 'AE',
+            templateUrl: 'home/directives/previewPanel.tpl.html',
+            scope: {
+                cat: '='
+            },
+            link: function(scope, element, attrs) {
+                var container = angular.element(document.getElementById('preview-container'));
+                var floater = angular.element(element[0].querySelector('.floater'));
+                var overlay = angular.element(document.getElementById('overlay'));
 
-        }
-    };
-});
+                scope.showInfo = "";
+
+                scope.clickHandler = function() {
+                    $rootScope.$broadcast('preview-click', scope.cat.id);
+                    if (scope.showInfo === "") {
+                        enable();
+                    } else {
+                        disable();
+                    }
+                };
+
+                scope.$on('preview-click', function(event, id) {
+                    if (scope.showInfo !== "" && id !== scope.cat.id) {
+                        disable();
+                    }
+                });
+
+                function enable() {
+                    scope.showInfo = "show-info";
+                    container.addClass('recessed');
+                    floater.addClass('float');
+                    overlay.addClass('displaying');
+                }
+
+                function disable() {
+                    scope.showInfo = "";
+                    container.removeClass('recessed');
+                    floater.removeClass('float');
+                    overlay.removeClass('displaying');
+                }
+            }
+        };
+    });
