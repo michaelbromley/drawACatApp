7382e1f9c0bd778292a0656b2408adae985e240f
Michael Bromley <michael@michaelbromley.co.uk>
2014-05-12 11:05:22 +0100

    include string-replace step when in watch mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -530,7 +530,7 @@ module.exports = function ( grunt ) {
                 files: [
                     '<%= app_files.js %>'
                 ],
-                tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
+                tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs', 'string-replace:build' ]
             },
 
             /**
